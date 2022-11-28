3b10bb9a71c043e8c206d070873d0efad6f238bb
sonaz87 <79515690+sonaz87@users.noreply.github.com>
2022-11-28 15:17:29 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for
+
+__winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
+__human_name__ = "templates"
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", page_name="Index")
+
+@app.route("/home")
+def home():
+    return redirect("/")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", page_name="About")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_name="Contact")
